9c55633bdf7430f2e915bec725f5068fc091132b
Aylaimran-02 <aylaimran02@gmail.com>
2025-10-19 22:19:10 +0500

    ..

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -1,10 +1,10 @@
 export const theme = {
   colors: {
-    primary: "#1d5e8e",
+    primary: "#6F31EE",
     secondary: "#2d82fe",
   },
   gradients: {
-    splash: ["#16461c", "#010101", "#0e2c43"], // dark green → black → dark blue
+    splash: ["#171717", "#6F31EE", "#0e2c43"], // dark green → black → dark blue
   },
   AIgradients: {
     linear: [ "#1D5E8E","#26E359"],
